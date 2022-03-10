1b3297643c920cd4b379769b2239f4844a73a604
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2022-03-10 14:21:28 +0100

    Add cmp setup for neovim and add snippets

--- .nvim/.config/nvim/lua/user/plugins.lua
+++ .nvim/.config/nvim/lua/user/plugins.lua
@@ -42,7 +42,23 @@ packer.init {
 -- Install your plugins here
 return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
+  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+
+  -- Colorschemes
   use "joshdick/onedark.vim"
+  use 'shaunsingh/solarized.nvim'
+  
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
 
   -- Automatically set up your configuration after cloning packer.nvim, must be after all plugins
