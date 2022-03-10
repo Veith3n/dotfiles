18af59ddd8e404cc136e6a6c1e9472a28b4363ff
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2022-03-10 10:25:13 +0100

    Set onedark color scheme in neovim

--- .nvim/.config/nvim/lua/user/plugins.lua
+++ .nvim/.config/nvim/lua/user/plugins.lua
@@ -42,6 +42,7 @@ packer.init {
 -- Install your plugins here
 return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
+  use "joshdick/onedark.vim"
 
 
   -- Automatically set up your configuration after cloning packer.nvim, must be after all plugins
