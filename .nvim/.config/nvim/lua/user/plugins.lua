604ee3706977e8f04f6241b1120bc7f95cb7dc5f
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2022-03-11 11:30:32 +0100

    Add and configure treesitter for neovim

--- .nvim/.config/nvim/lua/user/plugins.lua
+++ .nvim/.config/nvim/lua/user/plugins.lua
@@ -61,6 +61,10 @@ return packer.startup(function(use)
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
+ -- Treesitter
+  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+  use "p00f/nvim-ts-rainbow" 
+  
 
   -- Automatically set up your configuration after cloning packer.nvim, must be after all plugins
   if packer_bootstrap then
