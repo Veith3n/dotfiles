265eaec788146a77de659434330e3e2907a95fdb
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2022-05-09 18:29:57 +0200

    Set default color scheme based on the current hour

--- .nvim/.config/nvim/lua/user/colorscheme.lua
+++ .nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,4 +1,23 @@
-local colorscheme = "onedarker"
+
+local function determine_color ()
+  local dark_scheme = "onedarker"
+  local light_scheme = "solarized"
+
+  local light_scheme_start_time = 6
+  local light_scheme_end_time = 20
+
+  local current_hour = tonumber(os.date("%H"))
+
+  if current_hour > light_scheme_start_time then
+    if current_hour < light_scheme_end_time then
+      return light_scheme
+    end
+  else
+    return dark_scheme
+  end
+end
+
+local colorscheme = determine_color()
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
