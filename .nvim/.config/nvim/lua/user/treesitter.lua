d4079022e22ac20f43e959ae3c6ab80aa3ab0e95
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2022-05-11 14:19:09 +0200

    Migrate neovim and its plugins to 0.7 version

--- .nvim/.config/nvim/lua/user/treesitter.lua
+++ .nvim/.config/nvim/lua/user/treesitter.lua
@@ -4,9 +4,9 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false,  -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { "" }, -- List of parsers to ignore installing
+  ignore_install = { "phpdoc" }, -- https://github.com/filipekiss/nvim.lua/commit/eac8dfd8d393985e877d8b5dd1ad2d329a64ce60#diff-c184ac514122483c3cc73182b1a7c5d94ab7df2408f19f974c85454ebf498c58
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { "" }, -- list of language that will be disabled
