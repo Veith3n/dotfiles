4ee4d11f14dfe8ec8e2e3e2cf224b170f7474ae5
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2023-02-28 19:57:28 +0100

    Add windows dep file

---
+++ windows-dep.ps1
@@ -0,0 +1,18 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+
+choco install wiztree
+choco install steam
+choco install vscode
+choco install git
+choco install firefox
+choco install brave
+choco install hyper
+choco install discord
+choco install ccleaner
+choco install goggalaxy
+choco install ubisoft-connect
+choco install revo-uninstaller
+choco install driverbooster
+choco install teamviewer
+choco install virtualbox
