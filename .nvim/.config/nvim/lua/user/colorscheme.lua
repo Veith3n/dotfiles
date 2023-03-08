ffdd64943c40d7c5806db63409143f3feaa693ef
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2023-03-08 21:41:38 +0100

    Update script to correctly resolve motive

--- .nvim/.config/nvim/lua/user/colorscheme.lua
+++ .nvim/.config/nvim/lua/user/colorscheme.lua
@@ -2,41 +2,46 @@ local dark_scheme = "onedarker"
 local light_scheme = "solarized"
 
 local function determine_color_on_fixed_hours()
+	local light_scheme_start_time = 6
+	local light_scheme_end_time = 20
 
-  local light_scheme_start_time = 6
-  local light_scheme_end_time = 20
+	local current_hour = tonumber(os.date("%H"))
 
-  local current_hour = tonumber(os.date("%H"))
-
-  if current_hour > light_scheme_start_time then
-    if current_hour < light_scheme_end_time then
-      return light_scheme
-    else
-      return dark_scheme
-    end
-  else
-    return dark_scheme
-  end
+	if current_hour > light_scheme_start_time then
+		if current_hour < light_scheme_end_time then
+			return light_scheme
+		else
+			return dark_scheme
+		end
+	else
+		return dark_scheme
+	end
 end
 
-local function determine_color_based_on_OS_setting() -- works only for Macos
-  local Job = require("plenary.job")
+function determine_color_based_on_OS_setting() -- works only for Macos
+	local Job = require("plenary.job")
 
-  local j = Job:new({ command = "defaults", args = { "read", "-g", "AppleInterfaceStyle" } })
+	local j = Job:new({ command = "defaults", args = { "read", "-g", "AppleInterfaceStyle" } })
 
-  j:start()
+	j:start()
 
-  if j:result()[1] == "Dark" then
-    return dark_scheme
-  else
-    return light_scheme
-  end
+	local theme = j:result()[1]
+
+	-- return "test"
+	-- return if theme == "Dark" then dark_scheme else light_scheme end
+
+	if theme == "Dark" then
+		return dark_scheme
+	end
+
+	return light_scheme
 end
 
-local colorscheme = determine_color_based_on_OS_setting()
+-- local colorscheme = determine_color_based_on_OS_setting()
+local colorscheme = determine_color_on_fixed_hours()
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
+-- if not status_ok then
+-- vim.notify("colorscheme " .. colorscheme .. " not found!")
+-- return
+-- end
