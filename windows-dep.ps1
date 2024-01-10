2b7f4be55958affcb72fb5e88696854e43c95f6a
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2024-01-10 20:51:07 +0100

    refactor: Organize windows packages intro groups

--- windows-dep.ps1
+++ windows-dep.ps1
@@ -1,33 +1,13 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 
-choco install wiztree
-choco install steam
-choco install vscode
-choco install git
-choco install firefox
-choco install brave
-choco install hyper
-choco install discord
-choco install ccleaner
-choco install goggalaxy
-choco install ubisoft-connect
-choco install revo-uninstaller
-choco install driverbooster
-choco install teamviewer
-choco install virtualbox
-choco install hxd
-choco install geforce-experience
-choco install autoclicker
-choco install throttlestop
-choco install auto-dark-mode
-choco install kdeconnect-kde
-choco install cheatengine
-choco install bluestacks
+# utilities
 choco install 7zip
 choco install adobereader
-choco install vortex
-choco install vlc
+choco install auto-dark-mode
+choco install autoclicker
+choco install ccleaner
+choco install wiztree
 
 # debugging exe files (if they are malicous)
 choco install pestudio
@@ -37,3 +17,31 @@ choco install procexp
 
 # all startup processes
 choco install autoruns
+
+# dev related
+choco install git
+choco install teamviewer
+choco install virtualbox
+choco install vscode
+choco install hyper
+
+# games
+choco install bluestacks # android emulator
+choco install cheatengine
+choco install discord
+choco install goggalaxy
+choco install steam
+choco install ubisoft-connect
+choco install throttlestop
+choco install vortex # nexus mod manager
+
+## Browsers and Tools
+choco install brave
+choco install firefox
+choco install driverbooster
+choco install geforce-experience
+choco install hxd # hex file editor
+choco install kdeconnect-kde
+choco install revo-uninstaller
+choco install vlc # better media player
+
