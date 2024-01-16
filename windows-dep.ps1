e384ed49a5deaf095c52af786a5e521d82a9e4f8
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2024-01-16 21:41:35 +0100

    chore: Add comments

--- windows-dep.ps1
+++ windows-dep.ps1
@@ -1,5 +1,7 @@
+# choco install
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+# disable install confirmation
 choco feature enable -n allowGlobalConfirmation
 
 # utilities
