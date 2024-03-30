80791a7ce7e1e44acfbfdc460818edf4aaa72d65
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2024-03-30 17:43:30 +0100

    feat: Add script for toggling taskbar visibility

---
+++ windows/hide_unhide_taskbar.ps1
@@ -0,0 +1,1 @@
+$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;if($v[8] -eq 2){$v[8]=3}else{$v[8]=2};Set-ItemProperty -Path $p -Name Settings -Value $v;Stop-Process -f -ProcessName explorer
