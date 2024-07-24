510b86144a9287d3a3875af76c5aac5776534083
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2024-07-24 16:14:34 +0200

    feat: Add script

---
+++ script.sh
@@ -0,0 +1,11 @@
+brew install zsh     # neat CLI
+brew install gnu-sed # GNU based sed
+brew install bash    # install new version of bash
+echo "enter recovery mode and run csrutil disable, restart run set_new_bash_as_default, enter recovery mode again and run csrutil enable"
+brew install aspell # spell checker
+brew install gh     # github cli
+brew install jq
+brew install ngrok
+brew install dotenv-linter
+brew install htop
+brew install macs-fan-control
